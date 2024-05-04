1794db68dc45c1ab3081f1e7f5e14fa471ac3e23
HeyZammi <ayaanrashid960@gmail.com>
2024-05-04 12:12:35 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 connectDB().then(()=>{
-    app.listen(process.env.PORT,()=>{
+    app.listen(process.env.PORT || 6010,()=>{
         console.log("Server is running at PORT : ",process.env.PORT)
     })
 })
